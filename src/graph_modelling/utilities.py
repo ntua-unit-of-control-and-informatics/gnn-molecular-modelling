aa70a7f5ed472b580bfd9bf684e364862013a638
Giannis Pitoskas <jpitoskas@gmail.com>
2024-05-10 15:35:51 +0300

    Add custom loss to utils

--- src/graph_modelling/utilities.py
+++ src/graph_modelling/utilities.py
@@ -1,4 +1,5 @@
 import torch
+
 import sys
 import torch.nn as nn
 from abc import ABC, abstractmethod
@@ -198,4 +199,26 @@ def log_metrics(task, train_loss, metrics_output_tuple=None):
 
 
 
+class LabelSmoothingBCEWithLogitsLoss(nn.Module):
+    def __init__(self,
+                 smoothing=[0.0, 0.0],
+                 weight=None,
+                 size_average=None,
+                 reduce=None,
+                 reduction='mean',
+                 pos_weight=None,):
+        
+        super(LabelSmoothingBCEWithLogitsLoss, self).__init__()
+        
+        self.smoothing = smoothing
+        self.bce = nn.BCEWithLogitsLoss(weight, size_average, reduce, reduction, pos_weight)
+        
+
+    def forward(self, outputs, targets):
+        
+        label_smoothing = [torch.tensor(self.smoothing[0]).float(), torch.tensor(1-self.smoothing[1]).float()]
+        smoothed_labels = torch.where(targets < 0.5, label_smoothing[0], label_smoothing[1])
+        loss = self.bce(outputs, smoothed_labels)
+        
+        return loss
 
